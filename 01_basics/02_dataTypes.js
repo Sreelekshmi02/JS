3f109fb1b6b4532462c72c214a4adc5772db06bd
Sreelekshmi02 <152060360+Sreelekshmi02@users.noreply.github.com>
2024-07-31 07:47:15 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+let name = "Tony"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//num => standalone value(empty value)
+//undefined => not defined
+//symbol => unique
+
+
+// object 
+
+console.log(typeof "sree");
+console.log(typeof age);
+console.log(typeof null);  // object
+console.log(typeof undefined);  //undefined
